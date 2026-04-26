a3aaa3c861365a7820db0520a98b7b3b2bf917e9
Hao Nguyen <hao362k5@gmail.com>
2026-04-26 18:18:37 +0700

    temp

--- admin/src/main/java/GUI/Frame/Main.java
+++ admin/src/main/java/GUI/Frame/Main.java
@@ -193,6 +193,7 @@ public class Main extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(1300, 800));
+        getContentPane().setLayout(new java.awt.BorderLayout());
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setPreferredSize(new java.awt.Dimension(240, 470));
